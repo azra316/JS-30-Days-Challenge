452e3b64a4dccf19f84b919562078015f6f9165b
Azra <139526226+azra316@users.noreply.github.com>
2024-07-15 07:09:00 +0000

    Day-2 added

---
+++ DayTwo.js
@@ -0,0 +1,25 @@
+// Day-2: Operators
+//Activity One: Arithmatic operations
+
+// task-1
+const num1 = 10;
+const num2 = 3;
+//console.log(`the sum of ${num1} and ${num2} is ${num1 + num2}`);
+
+//Task -2
+const a = 56;
+const b = 45;
+//console.log(`the difference of ${a} and ${b} is ${a - b}`);
+
+// Task -3
+//console.log(`the product of ${a} and ${b} is ${a * b}`);
+
+//Task 4
+const Num1 = 15;
+const Num2 = 2;
+// console.log(`the quotient of ${Num1} divide by ${Num2} is ${Num1/Num2}`);
+// task-5
+// console.log(`the remainder of ${Num1} divide by ${Num2} is ${Num1%Num2}`);
+
+//Activity Two: Assignment operator
+//Task -4
